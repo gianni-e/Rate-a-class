99279c1e447644df40a50e1ee7c7c8c5ce47531c
gianni <gesposit16@stac.edu>
2020-02-26 09:29:18 -0500

    updates

--- app/src/main/java/com/example/rateaclass/rateCourse.java
+++ app/src/main/java/com/example/rateaclass/rateCourse.java
@@ -3,9 +3,11 @@ package com.example.rateaclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
 
-public class rateCourse extends AppCompatActivity {
-
+public class rateCourse extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
