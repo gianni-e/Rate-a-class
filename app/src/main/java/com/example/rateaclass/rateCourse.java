d2927735ee2e9cbc9e1e588ec1d17720302afa41
gianni <gesposit16@stac.edu>
2020-03-03 18:28:52 -0500

    updates

--- app/src/main/java/com/example/rateaclass/rateCourse.java
+++ app/src/main/java/com/example/rateaclass/rateCourse.java
@@ -1,16 +1,18 @@
 package com.example.rateaclass;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.view.View.OnClickListener;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class rateCourse extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate_course);
+    }
+
+    public void setCourseNumberSpinner()
+    {
+        
     }
 }
---
+++ app/src/main/java/com/example/rateaclass/rating.java
@@ -0,0 +1,66 @@
+package com.example.rateaclass;
+
+public class rating
+{
+    private int starRating;
+    private String courseName;
+    private String courseNumber;
+    private String comments;
+    private String instructor;
+
+    public rating(){}
+
+    public rating(int rating, String name, String number, String comment, String professor)
+    {
+        this.starRating = rating;
+        this.courseName = name;
+        this.courseNumber = number;
+        this.comments = comment;
+        this.instructor = professor;
+    }
+
+    public void setStarRating(int rating)
+    {
+        this.starRating = rating;
+    }
+    public int getStarRating()
+    {
+        return this.starRating;
+    }
+
+    public void setCourseName(String name)
+    {
+        this.courseName = name;
+    }
+    public String getCourseName()
+    {
+        return this.courseName;
+    }
+
+    public void setCourseNumber(String number)
+    {
+        this.courseNumber = number;
+    }
+    public String getCourseNumber()
+    {
+        return this.courseNumber;
+    }
+
+    public void setComments(String comment)
+    {
+        this.comments = comment;
+    }
+    public String getComments()
+    {
+        return this.comments;
+    }
+
+    public void setInstructor(String instructor)
+    {
+        this.instructor = instructor;
+    }
+    public String getInstructor()
+    {
+        return this.instructor;
+    }
+}
